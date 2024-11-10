4f2fe43d96b03feb2e30245999d6f62c63b91cf4
emkhizer <emkhizer007@gmail.com>
2024-11-10 19:14:34 +0500

    Add files via upload

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+// components/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-orange-500 w-full text-white flex justify-center py-4">
+      <p>© 2024 Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
